5cf06d90346ee8887b1decf48b4e5c55b9399032
Tim <69080064+HeyIam-Tim@users.noreply.github.com>
2022-06-29 11:00:34 +0500

    python-client-generated into root

---
+++ build/lib/swagger_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.analog_list_api import AnalogListApi
+from swagger_client.api.catalog_dt_api import CatalogDtApi
+from swagger_client.api.manufacturer_list_api import ManufacturerListApi
+from swagger_client.api.product_list_api import ProductListApi
